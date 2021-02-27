0c7ebc62234d92579de0f143635560df816bc7f2
mindnhand <albertqee@163.com>
2021-02-28 01:37:39 +0800

    add Chapter15

---
+++ Chapter15.TheDocumentationInterlude/docstrings.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#encoding=utf-8
+
+
+
+#------------------------------------
+#
+#------------------------------------
+
+
+"""
+Module documentation
+Words Go Here
+"""
+
+spam = 40
+
+def square(x):
+    """
+    funciton documentation
+    can we have your liver then?
+    """
+    return x ** 2       # square
+
+class Employee:
+    """
+    class documentation
+    """
+    pass
+
+print(square(4))
+print(square.__doc__)
